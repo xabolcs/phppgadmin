xabolcs/phppgadmin

commit e1a5b9c54f3ea112a0ce5b2f0f3a0ace9583a354

Makes display more plugin friendly
Plugins might need (at least Report) to set parameters in the URL and
use them in their code while working with display.php. This patch makes
sure their URL parameters are not lost while clicking on sort or pager
links when navigating in the result set.

Nice side effect: cleaner code (hopefuly with no bug...).

classes/Misc.php
@@ -1627,12 +1627,12 @@
 
 		/**
 		 * Do multi-page navigation.  Displays the prev, next and page options.
-		 * @param $page the page currently viewed
-		 * @param $pages the maximum number of pages
-		 * @param $url the url to refer to with the page number inserted
-		 * @param $max_width the number of pages to make available at any one time (default = 20)
+		 * @param $page - the page currently viewed
+		 * @param $pages - the maximum number of pages
+		 * @param $gets -  the parameters to include in the link to the wanted page
+		 * @param $max_width - the number of pages to make available at any one time (default = 20)
 		 */
-		function printPages($page, $pages, $url, $max_width = 20) {
+		function printPages($page, $pages, $gets, $max_width = 20) {
 			global $lang;
 
 			$window = 10;
@@ -1641,13 +1641,15 @@
 			if ($pages < 0) return;
 			if ($max_width <= 0) return;
 
+			unset ($get['page']);
+			$url = http_build_query($gets);
+
 			if ($pages > 1) {
 				echo "<p style=\"text-align: center\">\n";
 				if ($page != 1) {
-					$temp = str_replace('%s', 1, $url);
-					echo "<a class=\"pagenav\" href=\"{$temp}\">{$lang['strfirst']}</a>\n";
-					$temp = str_replace('%s', $page - 1, $url);
-					echo "<a class=\"pagenav\" href=\"{$temp}\">{$lang['strprev']}</a>\n";
+					echo "<a class=\"pagenav\" href=\"?{$url}&amp;page=1\">{$lang['strfirst']}</a>\n";
+					$temp = $page - 1;
+					echo "<a class=\"pagenav\" href=\"?{$url}&amp;page={$temp}\">{$lang['strprev']}</a>\n";
 				}
 
 				if ($page <= $window) {
@@ -1669,15 +1671,13 @@
 				$max_page = min($max_page, $pages);
 
 				for ($i = $min_page; $i <= $max_page; $i++) {
-					$temp = str_replace('%s', $i, $url);
-					if ($i != $page) echo "<a class=\"pagenav\" href=\"{$temp}\">$i</a>\n";
+					if ($i != $page) echo "<a class=\"pagenav\" href=\"?{$url}&amp;page={$i}\">$i</a>\n";
 					else echo "$i\n";
 				}
 				if ($page != $pages) {
-					$temp = str_replace('%s', $page + 1, $url);
-					echo "<a class=\"pagenav\" href=\"{$temp}\">{$lang['strnext']}</a>\n";
-					$temp = str_replace('%s', $pages, $url);
-					echo "<a class=\"pagenav\" href=\"{$temp}\">{$lang['strlast']}</a>\n";
+					$temp = $page + 1;
+					echo "<a class=\"pagenav\" href=\"?{$url}&amp;page={$temp}\">{$lang['strnext']}</a>\n";
+					echo "<a class=\"pagenav\" href=\"?{$url}&amp;page={$pages}\">{$lang['strlast']}</a>\n";
 				}
 				echo "</p>\n";
 			}

display.php
@@ -294,7 +294,7 @@
 	/* Print table header cells 
 	 * @param $sortLink must be urlencoded already
 	 * */
-	function printTableHeaderCells(&$rs, $sortLink, $withOid) {
+	function printTableHeaderCells(&$rs, $args, $withOid) {
 		global $misc, $data, $conf;
 		$j = 0;
 
@@ -310,12 +310,19 @@
 				echo "<th class=\"data\">", $misc->printVal($finfo->name), "</th>\n";
 			}
 			else {
-				echo "<th class=\"data\"><a href=\"display.php?{$sortLink}&amp;sortkey=", ($j + 1), "&amp;sortdir=";
+				$args['page'] = $_REQUEST['page'];
+				$args['sortkey'] = $j + 1;
 				// Sort direction opposite to current direction, unless it's currently ''
-				echo ($_REQUEST['sortdir'] == 'asc' && $_REQUEST['sortkey'] == ($j + 1)) ? 'desc' : 'asc';
-				echo "&amp;strings=", urlencode($_REQUEST['strings']), 
-					"&amp;page=" . urlencode($_REQUEST['page']), "\">", 
-					$misc->printVal($finfo->name), "</a></th>\n";
+				$args['sortdir'] = (
+					$_REQUEST['sortdir'] == 'asc'
+					and $_REQUEST['sortkey'] == ($j + 1)
+				) ? 'desc' : 'asc';
+
+				$sortLink = http_build_query($args);
+
+				echo "<th class=\"data\"><a href=\"?{$sortLink}\">"
+					, $misc->printVal($finfo->name)
+					, "</a></th>\n";
 			}
 			$j++;
 		}
@@ -454,6 +461,7 @@
 			}
 		} else {
 			$misc->printTitle($lang['strqueryresults']);
+			/*we comes from sql.php, $_SESSION['sqlquery'] has been set there */
 			$type = 'QUERY';
 		}
 
@@ -490,47 +498,17 @@
 
 		$fkey_information =& getFKInfo();
 
-		// Build strings for GETs
-		$gets = $misc->href;
-		if (isset($object)) $gets .= "&amp;" . urlencode($subject) . '=' . urlencode($object);
-		if (isset($subject)) $gets .= "&amp;subject=" . urlencode($subject);
-		if (isset($_REQUEST['query'])) $gets .= "&amp;query=" . urlencode($_REQUEST['query']);
-		if (isset($_REQUEST['count'])) $gets .= "&amp;count=" . urlencode($_REQUEST['count']);
-		if (isset($_REQUEST['return'])) $gets .= "&amp;return=" . urlencode($_REQUEST['return']);
-		if (isset($_REQUEST['search_path'])) $gets .= "&amp;search_path=" . urlencode($_REQUEST['search_path']);
-		if (isset($_REQUEST['table'])) $gets .= "&amp;table=" . urlencode($_REQUEST['table']);
-
-		// Build strings for GETs in array
-		$_gets = array(
-			'server' => $_REQUEST['server'],
-			'database' => $_REQUEST['database']
-		);
-
-		if (isset($_REQUEST['schema'])) $_gets['schema'] = $_REQUEST['schema'];
-		if (isset($object)) $_gets[$subject] = $object;
-		if (isset($subject)) $_gets['subject'] = $subject;
-		if (isset($_REQUEST['query'])) $_gets['query'] = $_REQUEST['query'];
-		if (isset($_REQUEST['count'])) $_gets['count'] = $_REQUEST['count'];
-		if (isset($_REQUEST['return'])) $_gets['return'] = $_REQUEST['return'];
-		if (isset($_REQUEST['search_path'])) $_gets['search_path'] = $_REQUEST['search_path'];
-		if (isset($_REQUEST['table'])) $_gets['table'] = $_REQUEST['table'];
-
-		// This string just contains sort info
-		$getsort = "sortkey=" . urlencode($_REQUEST['sortkey']) .
-			"&amp;sortdir=" . urlencode($_REQUEST['sortdir']);
-
-		// This array only contains sort info
-		$_getsort = array(
-			'sortkey' => $_REQUEST['sortkey'],
-			'sortdir' => $_REQUEST['sortdir']
-		);
+		// Build array OF parameters for sorts/pages links
+		$_gets = $_REQUEST;
+		unset($_gets['query']);
+		unset($_gets['MAX_FILE_SIZE']);
 
 		if ($save_history && is_object($rs) && ($type == 'QUERY')) //{
 			$misc->saveScriptHistory($_REQUEST['query']);
 
 		if (is_object($rs) && $rs->recordCount() > 0) {
 			// Show page navigation
-			$misc->printPages($_REQUEST['page'], $max_pages, "display.php?page=%s&amp;{$gets}&amp;{$getsort}&amp;nohistory=t&amp;strings=" . urlencode($_REQUEST['strings']));
+			$misc->printPages($_REQUEST['page'], $max_pages, $_gets);
 
 			echo "<table id=\"data\">\n<tr>";
 
@@ -556,7 +534,7 @@
 								'action' => 'confeditrow',
 								'strings' => $_REQUEST['strings'],
 								'page' => $_REQUEST['page'],
-							), $_gets, $_getsort)
+							), $_gets)
 						)
 					)
 				),
@@ -569,7 +547,7 @@
 								'action' => 'confdelrow',
 								'strings' => $_REQUEST['strings'],
 								'page' => $_REQUEST['page'],
-							), $_gets, $_getsort)
+							), $_gets)
 						)
 					)
 				),
@@ -583,7 +561,7 @@
 			foreach (array_keys($actions['actionbuttons']) as $action) {
 				$actions['actionbuttons'][$action]['attr']['href']['urlvars'] = array_merge(
 					$actions['actionbuttons'][$action]['attr']['href']['urlvars'],
-					$_gets, $_getsort
+					$_gets
 				);
 			}
 
@@ -596,7 +574,7 @@
 				echo "<th colspan=\"{$colspan}\" class=\"data\">{$lang['stractions']}</th>\n";
 
 			/* we show OIDs only if we are in TABLE or SELECT type browsing */
-			printTableHeaderCells($rs, $gets, isset($object));
+			printTableHeaderCells($rs, $_gets, isset($object));
 
 			echo "</tr>\n";
 
@@ -655,7 +633,7 @@
 
 			echo "<p>", $rs->recordCount(), " {$lang['strrows']}</p>\n";
 			// Show page navigation
-			$misc->printPages($_REQUEST['page'], $max_pages, "display.php?page=%s&amp;{$gets}&amp;{$getsort}&amp;strings=" . urlencode($_REQUEST['strings']));
+			$misc->printPages($_REQUEST['page'], $max_pages, $_gets);
 		}
 		else echo "<p>{$lang['strnodata']}</p>\n";
 
@@ -707,7 +685,7 @@
 					'href' => array (
 						'url' => 'display.php',
 						'urlvars' => array_merge(
-							$_gets, $_getsort,
+							$_gets,
 							array (
 								'strings' => 'collapsed',
 								'page' => $_REQUEST['page']
@@ -722,7 +700,7 @@
 					'href' => array (
 						'url' => 'display.php',
 						'urlvars' => array_merge(
-							$_gets, $_getsort,
+							$_gets,
 							array (
 								'strings' => 'expanded',
 								'page' => $_REQUEST['page']
@@ -789,7 +767,7 @@
 				'href' => array (
 					'url' => 'display.php',
 					'urlvars' => array_merge(
-						$_gets, $_getsort,
+						$_gets,
 						array(
 							'strings' => $_REQUEST['strings'],
 							'page' => $_REQUEST['page']

commit 0693ba765d1644a01a2333904376765d44da9a78

Fix pagination issue reported by phb07 on sf.net
See thread "[ppa-dev] ppa51 bug report: pagination on select report":
  http://sourceforge.net/mailarchive/message.php?msg_id=31062872

Bug introduced in commit e1a5b9c54f3e by ioguix on 2012-08-17.

classes/Misc.php
@@ -1648,9 +1648,10 @@
 		 * @param $page - the page currently viewed
 		 * @param $pages - the maximum number of pages
 		 * @param $gets -  the parameters to include in the link to the wanted page
+		 * @param $script - the script where the parameters should passed to (default = "")
 		 * @param $max_width - the number of pages to make available at any one time (default = 20)
 		 */
-		function printPages($page, $pages, $gets, $max_width = 20) {
+		function printPages($page, $pages, $gets, $script = "", $max_width = 20) {
 			global $lang;
 
 			$window = 10;
@@ -1665,9 +1666,9 @@
 			if ($pages > 1) {
 				echo "<p style=\"text-align: center\">\n";
 				if ($page != 1) {
-					echo "<a class=\"pagenav\" href=\"?{$url}&amp;page=1\">{$lang['strfirst']}</a>\n";
+					echo "<a class=\"pagenav\" href=\"{$script}?{$url}&amp;page=1\">{$lang['strfirst']}</a>\n";
 					$temp = $page - 1;
-					echo "<a class=\"pagenav\" href=\"?{$url}&amp;page={$temp}\">{$lang['strprev']}</a>\n";
+					echo "<a class=\"pagenav\" href=\"{$script}?{$url}&amp;page={$temp}\">{$lang['strprev']}</a>\n";
 				}
 
 				if ($page <= $window) {
@@ -1689,13 +1690,13 @@
 				$max_page = min($max_page, $pages);
 
 				for ($i = $min_page; $i <= $max_page; $i++) {
-					if ($i != $page) echo "<a class=\"pagenav\" href=\"?{$url}&amp;page={$i}\">$i</a>\n";
+					if ($i != $page) echo "<a class=\"pagenav\" href=\"{$script}?{$url}&amp;page={$i}\">$i</a>\n";
 					else echo "$i\n";
 				}
 				if ($page != $pages) {
 					$temp = $page + 1;
-					echo "<a class=\"pagenav\" href=\"?{$url}&amp;page={$temp}\">{$lang['strnext']}</a>\n";
-					echo "<a class=\"pagenav\" href=\"?{$url}&amp;page={$pages}\">{$lang['strlast']}</a>\n";
+					echo "<a class=\"pagenav\" href=\"{$script}?{$url}&amp;page={$temp}\">{$lang['strnext']}</a>\n";
+					echo "<a class=\"pagenav\" href=\"{$script}?{$url}&amp;page={$pages}\">{$lang['strlast']}</a>\n";
 				}
 				echo "</p>\n";
 			}

display.php
@@ -527,7 +527,7 @@
 
 		if (is_object($rs) && $rs->recordCount() > 0) {
 			// Show page navigation
-			$misc->printPages($_REQUEST['page'], $max_pages, $_gets);
+			$misc->printPages($_REQUEST['page'], $max_pages, $_gets, 'display.php');
 
 			echo "<table id=\"data\">\n<tr>";
 
@@ -653,7 +653,7 @@
 
 			echo "<p>", $rs->recordCount(), " {$lang['strrows']}</p>\n";
 			// Show page navigation
-			$misc->printPages($_REQUEST['page'], $max_pages, $_gets);
+			$misc->printPages($_REQUEST['page'], $max_pages, $_gets, 'display.php');
 		}
 		else echo "<p>{$lang['strnodata']}</p>\n";
 